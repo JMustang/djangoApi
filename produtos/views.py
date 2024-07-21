09de215fe13b77a54f74c4b9f1b9bafd2baff648
Junior <eecfredes@gmail.com>
2024-07-21 14:54:36 -0300

    :rocket: Adicionando o template a view

--- produtos/views.py
+++ produtos/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ver_produto(request):
-    return HttpResponse("Ver produto")
+    return render(request, "ver_produto.html", {"nome": "Alice"})
 
 
 def inserir_produto(request):
