364207f6061017caf64026f93260128e429bcc5b
Junior <eecfredes@gmail.com>
2024-07-22 21:29:36 -0300

    :rocket: funcao que recebe e retorna informacoes do formulario

--- produtos/views.py
+++ produtos/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ver_produto(request):
-    return render(request, "ver_produto.html", {"nome": "Alice"})
+    if request.method == "GET":
+        return render(request, "ver_produto.html", {"nome": "Alice"})
+    elif request.method == "POST":
+        nome = request.POST.get("nome")
+        idade = request.POST.get("idade")
+        return HttpResponse(f"Ola {nome}, sua idade é {idade} anos!")
 
 
 def inserir_produto(request):
